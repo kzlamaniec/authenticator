c389c37282c8eaf917062484a358b8a9f488b7e1
kzlamaniec <klaudia.zlamaniec314@gmail.com>
2022-01-05 11:29:37 +0100

    first commit

---
+++ authenticator.rb
@@ -0,0 +1,14 @@
+users = [
+           {username: "klaudia", password: "password1"},
+           {username: "milosz", password: "password2"},
+           {username: "grzegorz", password: "password3"},
+           {username: "ewa", password: "password4"}, 
+           {username: "kamila", password: "password5"}
+        ]
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+
+#Hash, Array, Branching, while loops, and designing program execution flow
